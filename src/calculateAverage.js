f7fd36109e3f321dd6ff4057035362089f90ed11
Valentyn Krushynin <krushunin89@gmail.com>
2020-06-01 22:38:17 +0300

    refactored some statement

--- src/calculateAverage.js
+++ src/calculateAverage.js
@@ -8,7 +8,11 @@ function applyCalculateAverage() {
   [].__proto__.calculateAverage = function(options) {
     if (options === undefined) {
       return this.reduce((a, c) => a + c, 0) / this.length;
-    } else if (typeof options === 'string' || options === null) {
+    } else if (typeof options === 'string'
+    || options === null
+    || typeof options.accumulator === 'string'
+    || (!this.some((el) => el
+      .hasOwnProperty(options.propertyName)) && options.propertyName)) {
       return;
     }
 
@@ -17,18 +21,13 @@ function applyCalculateAverage() {
     || ((sum, current) => sum + current[property]);
     let average;
 
-    if (!this.some((el) => el.hasOwnProperty(property)) && property) {
-      return undefined;
-    } else if (this.some((el) => el.hasOwnProperty(property)) && property) {
+    if (this.some((el) => el.hasOwnProperty(property)) && property) {
       const filteredItems = this.filter(el => el.hasOwnProperty(property));
 
       average = filteredItems.reduce(itemsSum, 0) / filteredItems.length;
 
       return !Number.isNaN(average) ? average : undefined;
     } else if (options.accumulator) {
-      if (typeof options.accumulator !== 'function') {
-        return;
-      }
       average = this.map(options.accumulator);
 
       return average.reduce((a, c) => a + c, 0) / average.length;
